a660733d55099966da34831150a769a21494cc3f
Shawn Mclean <shawnrmclean@gmail.com>
2016-12-31 21:17:41 -0500

    Added NLog and Sentry event.

--- src/NLog.Targets.Sentry.Core/SentryTarget.cs
+++ src/NLog.Targets.Sentry.Core/SentryTarget.cs
@@ -1,10 +1,117 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// ReSharper disable CheckNamespace
 
-namespace NLog.Targets.Sentry.Core
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using NLog.Common;
+using NLog.Config;
+using SharpRaven.Core;
+using SharpRaven.Core.Data;
+
+namespace NLog.Targets
+// ReSharper restore CheckNamespace
 {
-  public class SentryTarget
+  [Target("Sentry")]
+  public class SentryTarget : TargetWithLayout
   {
+    private Dsn dsn;
+    private readonly Lazy<IRavenClient> client;
+
+    /// <summary>
+    /// Map of NLog log levels to Raven/Sentry log levels
+    /// </summary>
+    protected static readonly IDictionary<LogLevel, ErrorLevel> LoggingLevelMap = new Dictionary<LogLevel, ErrorLevel>
+        {
+            {LogLevel.Debug, ErrorLevel.Debug},
+            {LogLevel.Error, ErrorLevel.Error},
+            {LogLevel.Fatal, ErrorLevel.Fatal},
+            {LogLevel.Info, ErrorLevel.Info},
+            {LogLevel.Trace, ErrorLevel.Debug},
+            {LogLevel.Warn, ErrorLevel.Warning},
+        };
+
+    /// <summary>
+    /// The DSN for the Sentry host
+    /// </summary>
+    [RequiredParameter]
+    public string Dsn
+    {
+      get { return dsn == null ? null : dsn.ToString(); }
+      set { dsn = new Dsn(value); }
+    }
+
+    /// <summary>
+    /// Determines whether events with no exceptions will be send to Sentry or not
+    /// </summary>
+    public bool IgnoreEventsWithNoException { get; set; }
+
+    /// <summary>
+    /// Determines whether event properites will be sent to sentry as Tags or not
+    /// </summary>
+    public bool SendLogEventInfoPropertiesAsTags { get; set; }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public SentryTarget()
+    {
+      client = new Lazy<IRavenClient>(() => new RavenClient(dsn));
+    }
+
+    /// <summary>
+    /// Internal constructor, used for unit-testing
+    /// </summary>
+    /// <param name="ravenClient">A <see cref="IRavenClient"/></param>
+    internal SentryTarget(IRavenClient ravenClient) : this()
+    {
+      client = new Lazy<IRavenClient>(() => ravenClient);
+    }
+    
+    /// <summary>
+    /// Writes logging event to the log target.
+    /// </summary>
+    /// <param name="logEvent">Logging event to be written out.</param>
+    protected override void Write(LogEventInfo logEvent)
+    {
+      try
+      {
+        var tags = SendLogEventInfoPropertiesAsTags
+            ? logEvent.Properties.ToDictionary(x => x.Key.ToString(), x => x.Value.ToString())
+            : null;
+
+        var extras = SendLogEventInfoPropertiesAsTags
+            ? null
+            : logEvent.Properties.ToDictionary(x => x.Key.ToString(), x => x.Value.ToString());
+
+        client.Value.Logger = logEvent.LoggerName;
+
+        // If the log event did not contain an exception and we're not ignoring
+        // those kinds of events then we'll send a "Message" to Sentry
+        if (logEvent.Exception == null && !IgnoreEventsWithNoException)
+        {
+          var sentryEvent = new SentryEvent(new SentryMessage(Layout.Render(logEvent)))
+          {
+            Level = LoggingLevelMap[logEvent.Level],
+            Extra = extras
+          };
+          client.Value.CaptureAsync(sentryEvent).RunSynchronously();
+        }
+        else if (logEvent.Exception != null)
+        {
+          var sentryEvent = new SentryEvent(logEvent.Exception)
+          {
+            Level = LoggingLevelMap[logEvent.Level],
+            Extra = extras,
+            Message = new SentryMessage(logEvent.FormattedMessage)
+          };
+          client.Value.CaptureAsync(sentryEvent).RunSynchronously();
+        }
+      }
+      catch (Exception e)
+      {
+        InternalLogger.Error("Unable to send Sentry request: {0}", e.Message);
+      }
+    }
   }
 }
